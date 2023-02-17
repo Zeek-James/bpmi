06bdf5da79b2dfee7d291caba5bb5e4592d530f8
Zeek James <65875140+Zeek-James@users.noreply.github.com>
2023-02-17 18:47:02 +0100

    Update _app.tsx

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,20 +1,12 @@
 import "../styles/globals.css";
 import type { AppProps } from "next/app";
-import LayoutType from "../types/LayoutType";
-import { ReactElement } from "react";
+import { Layout } from "../components";
 
-type AppLayoutProps = AppProps & {
-  Component: LayoutType;
-  pageProps: any;
-};
-
-export default function App({ Component, pageProps }: AppLayoutProps) {
-  const Layout =
-    Component.layout || ((children: ReactElement) => <>{children}</>);
-
+export default function App({ Component, pageProps }: AppProps) {
   return (
     <Layout>
       <Component {...pageProps} />
     </Layout>
   );
 }
+
